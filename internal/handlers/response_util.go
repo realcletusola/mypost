22966bdbe9547f57096337d81823e8bc089081f3
cletus <cletushunsu@gmail.com>
2025-01-28 09:09:59 +0100

    Created response util

---
+++ internal/handlers/response_util.go
@@ -0,0 +1,21 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// Create function to decode Json
+func decodeJSONBody(w http.ResponseWriter, r *http.Request, dest interface{}) error {
+	decoder := json.NewDecoder(r.Body)
+	defer r.Body.Close()
+	return decoder.Decode(dest)
+}
+
+// Create function to encode Json
+func encodeJSONBody(w http.ResponseWriter, data interface{}) {
+	w.Header().set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+	}
+}
