6cf3e3d0671176008653b73a277e34924e2be38d
cletus <cletushunsu@gmail.com>
2025-01-28 09:10:44 +0100

    created routes

---
+++ internal/routes/routers.go
@@ -0,0 +1,13 @@
+package routes 
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/realcletusola/mypost/handlers"
+)
+
+func SetupRouter() *chi.Mux {
+	r := chi.NewRouter()
+	r.Post("/posts", handlers.CreatePost)
+	r.Get("/posts", handlers.GetPosts)
+	r.Get("/posts/{id", handlers.GetPostByID)
+}
